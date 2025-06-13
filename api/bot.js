0f2dadaffcd8019f0ee5b7c7dcc09cd7432a57e5
y0йz <59342421+y0lz@users.noreply.github.com>
2025-06-13 14:08:39 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -4,7 +4,9 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.command('start', (ctx) => ctx.reply('✅ Вебхук работает!'));
 
 // Важно: экспортируйте webhookCallback
-module.exports = bot.webhookCallback('/api/bot');
+module.exports = bot.webhookCallback('/api/bot', {
+    secretToken: process.env.SECRET_TOKEN
+  });
 
 bot.catch((err, ctx) => {
     console.error(`Ошибка для ${ctx.update.update_id}:`, err);
