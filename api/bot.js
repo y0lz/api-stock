2b13140db40a37adc2d58d2692fa60ac85e235fe
y0йz <59342421+y0lz@users.noreply.github.com>
2025-06-13 14:04:22 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -8,3 +8,7 @@ bot.command('start', (ctx) => {
 
 // Важно: только для Vercel (без bot.launch())
 module.exports = bot.webhookCallback('/api/bot');
+
+bot.catch((err, ctx) => {
+    console.error(`Ошибка для ${ctx.update.update_id}:`, err);
+  });
