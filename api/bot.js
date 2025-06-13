9beaa71fe087b251d0534163ce24fec335b2851f
y0йz <59342421+y0lz@users.noreply.github.com>
2025-06-13 13:44:35 +0300

    brrbr

--- api/bot.js
+++ api/bot.js
@@ -1,7 +1,15 @@
 const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command('start', (ctx) => ctx.reply('Welcome!'));
+bot.command('start', (ctx) => {
+  ctx.reply('Бот успешно запущен! 🚀');
+});
 
-// Для Vercel Serverless
+// Для локального тестирования (polling)
+bot.launch()
+  .then(() => console.log('Бот запущен в polling режиме'))
+  .catch(err => console.error('Ошибка запуска:', err));
+
 module.exports = bot.webhookCallback('/api/bot');
